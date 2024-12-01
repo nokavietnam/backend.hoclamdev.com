775595a0ee0a6742e68ce5f96ac5bae3d1437612
noka <noka.hieunt@gmail.com>
2024-12-01 12:14:23 +0700

    update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,18 @@
-import imp
-import os
+# import imp
+# import os
+# import sys
+
+
+# sys.path.insert(0, os.path.dirname(__file__))
+
+# wsgi = imp.load_source('wsgi', 'app.py')
+# application = wsgi.app
+
+import importlib.util
 import sys
 
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-wsgi = imp.load_source('wsgi', 'app.py')
-application = wsgi.app
+spec = importlib.util.spec_from_file_location("app", "app.py")
+module = importlib.util.module_from_spec(spec)
+sys.modules["app"] = module
+spec.loader.exec_module(module)
+application = module.application
