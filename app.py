cf98bfd0a8b15c251f2ccbc201f4b057be0b2b15
nokavietnam <noka.hieunt@gmail.com>
2024-12-01 15:51:07 +0700

    update

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_pyfile('settings.py')
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/env')
+def test_env():
+    return f'{os.environ['MYSQL_USER']} - {os.environ['MYSQL_PASSWORD']}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ settings.py
@@ -0,0 +1,4 @@
+from os import environ
+
+MYSQL_USER=environ.get('MYSQL_USER')
+MYSQL_PASSWORD=environ.get('MYSQL_PASSWORD')
